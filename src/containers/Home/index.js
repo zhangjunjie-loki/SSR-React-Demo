6f47021e8bd8042c4c1cd611d3e5b0d5e4ff6310
zhangjunjie0730 <317574086@qq.com>
2022-01-26 09:58:33 +0800

    fix:detail code

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -5,6 +5,7 @@ import { Helmet } from "react-helmet"
 import styles from "./style.css"
 import { getHomeList } from "./store/actions"
 import useStyles from "../../hooks/useStyles"
+const Alldata={}
 
 const Home = (props) => {
   // 处理css问题
@@ -30,6 +31,7 @@ const Home = (props) => {
         <meta charSet="utf-8" />
         <title>react-ssr|首页-丰富多彩的资讯</title>
       </Helmet>
+      <div>{Alldata.testData}</div>
       <div className={styles.container}>
         {
           homeList.map((item)=>{
@@ -42,6 +44,7 @@ const Home = (props) => {
 }
 Home.loadData = (store) => {
   // 该函数负责在服务端渲染前把该路由需要的数据提前加载好！
+  Alldata.testData = "testdata-erwerwe"
   return store.dispatch(getHomeList())
 }
 
